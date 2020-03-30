5f41261cf11f6b6780faf2716770fde9dea6f180
Marekt94 <marekt94@gmail.com>
2020-03-31 01:04:06 +0200

    Pole ilość sztuk/opakowań/itd.
    - dodano pole, gdzie podaje się sztuki/jednostki/opakowania i odpowiednio przelicza na wagę,
    - niedostępne, gdy brak wagi jednostki,
    - nie ma obsługi błędów w przypadku podania liter

--- Nourishment/src/main/java/View/WagaProduktuPanel.java
+++ Nourishment/src/main/java/View/WagaProduktuPanel.java
@@ -6,10 +6,13 @@
 package View;
 
 import Entities.ProduktyWPotrawie;
+import static Global.GlobalConfig.NULL_SIGN;
 import Global.GlobalFun;
 import Interfaces.MyPanelInterface;
 import View.BasicView.BasePanel;
 import View.BasicView.KonfigView;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.List;
 
 /**
@@ -38,19 +41,50 @@ public class WagaProduktuPanel extends BasePanel {
 
         lblWagaProduktu = new javax.swing.JLabel();
         edtWagaProdruktu = new javax.swing.JTextField();
+        lblIlosc = new javax.swing.JLabel();
+        edtIlosc = new javax.swing.JTextField();
 
-        setPreferredSize(new java.awt.Dimension(233, 22));
-        setLayout(new java.awt.GridLayout(1, 2, 5, 5));
+        setLayout(new java.awt.GridLayout(2, 2, 5, 5));
 
         lblWagaProduktu.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lblWagaProduktu.setText("Waga produktu");
         add(lblWagaProduktu);
+
+        edtWagaProdruktu.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                edtWagaProdruktuKeyReleased(evt);
+            }
+        });
         add(edtWagaProdruktu);
+
+        lblIlosc.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lblIlosc.setText("Liczba sztuk/opakowań/jednostek");
+        lblIlosc.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        add(lblIlosc);
+
+        edtIlosc.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                edtIloscKeyReleased(evt);
+            }
+        });
+        add(edtIlosc);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void edtWagaProdruktuKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_edtWagaProdruktuKeyReleased
+        Double value = (Double) GlobalFun.bind(edtWagaProdruktu, Double.class);
+        GlobalFun.bind(evaluateIlosc(value), edtIlosc);
+    }//GEN-LAST:event_edtWagaProdruktuKeyReleased
+
+    private void edtIloscKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_edtIloscKeyReleased
+        Double ilosc = (Double) GlobalFun.bind(edtIlosc, Double.class);
+        GlobalFun.bind(evaluateWaga(ilosc), edtWagaProdruktu);
+    }//GEN-LAST:event_edtIloscKeyReleased
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField edtIlosc;
     private javax.swing.JTextField edtWagaProdruktu;
+    private javax.swing.JLabel lblIlosc;
     private javax.swing.JLabel lblWagaProduktu;
     // End of variables declaration//GEN-END:variables
 
@@ -58,10 +92,30 @@ public class WagaProduktuPanel extends BasePanel {
     public <E> void unpack(E object) {
         prodWPotr = (ProduktyWPotrawie) object;
         GlobalFun.bind(prodWPotr.getIloscWG(), edtWagaProdruktu);
+        if (prodWPotr.getIdProduktu().getWagaJednostki().equals(0.0)){
+            edtIlosc.setEnabled(false);
+        }
+        else{
+            edtIlosc.setEnabled(true);
+        }
+        GlobalFun.bind(evaluateIlosc(prodWPotr.getIloscWG()), edtIlosc);
     }
 
     @Override
     public void pack() {
         prodWPotr.setIloscWG((Double) GlobalFun.bind(edtWagaProdruktu, Double.class));
+    }
+    
+    private Double evaluateWaga(Double ilosc){
+        return ilosc * prodWPotr.getIdProduktu().getWagaJednostki();
+    }
+    
+    private Double evaluateIlosc(Double waga){
+        if (prodWPotr.getIdProduktu().getWagaJednostki().equals(0.0)){
+            return 0.0;
+        } 
+        else{
+            return waga / prodWPotr.getIdProduktu().getWagaJednostki();   
+        }
     }
 }
